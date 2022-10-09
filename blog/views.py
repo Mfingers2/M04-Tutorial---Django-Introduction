2e8ff203a0ab29621dcdc6ae4d63045187c1e814
Mfingers2 <Mfingers@ivytevh.edu>
2022-10-09 15:22:00 -0500

    adding new files

--- blog/views.py
+++ blog/views.py
@@ -14,6 +14,12 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
+def post_draft_list(request):
+    posts = Post.objects.filter(
+        published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
 @login_required
 def post_new(request):
     if request.method == "POST":
@@ -21,7 +27,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -42,7 +47,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
